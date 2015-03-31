9796e24a2e3a5fadf337cfb341f5b48a0ff150a6
Lorie Ransom <lorie@ransomdesign.com>
2015-03-30 23:15:57 -0700

    Expanded functionality
    - added bootstrap for ui prettiness
    - added bootstrap kitchen sink for reference
    - set up module to filter by size, color and behavior

--- lodash_test.js
+++ lodash_test.js
@@ -3,20 +3,21 @@
 
 	//dummy data
 	var selectedSize = 'tiny';
-	var selectedColors = ["red", "yellow", "Purple", "Plaid"];
+	var selectedColors = ["red"];
+	var selectedBehavior = 'soaring';
 	
-	// var globalBirdData =  [{
-	// 	//initializing this variable with data - for testing whether this is 
-	// 	//successfully replaced by the .getJSON call
-	// 	"excerpt" : "Looking through the code you may notice that a new function named init() was added that wasn’t in the previous examples. It’s responsible for accepting any initialization data that the Calculator object needs to work correctly. As soon as the page loads the Calculator object is created but init() needs to be called to pass two HTML elements that it interacts with. An example of calling init() is shown next:",
-	// 	"id" : 1
-	// 	},
-	// 	{
-	// 	"excerpt" : "The Revealing Module Pattern is currently my favorite pattern out there for structuring JavaScript code mainly because it’s easy to use, very readable (which is important for maintenance), and provides a simple way to expose public members to consumers. What if we could combine this pattern with the Prototype Pattern though to get the benefits provided by prototyping? That’ll be the subject of my next post",
-	// 	"id" : 2
-	// 	}
-	// ];
-	// 
+/*	var globalBirdData =  [{
+		//initializing this variable with data - for testing whether this is 
+		//successfully replaced by the .getJSON call
+		"excerpt" : "Looking through the code you may notice that a new function named init() was added that wasn’t in the previous examples. It’s responsible for accepting any initialization data that the Calculator object needs to work correctly. As soon as the page loads the Calculator object is created but init() needs to be called to pass two HTML elements that it interacts with. An example of calling init() is shown next:",
+		"id" : 1
+		},
+		{
+		"excerpt" : "The Revealing Module Pattern is currently my favorite pattern out there for structuring JavaScript code mainly because it’s easy to use, very readable (which is important for maintenance), and provides a simple way to expose public members to consumers. What if we could combine this pattern with the Prototype Pattern though to get the benefits provided by prototyping? That’ll be the subject of my next post",
+		"id" : 2
+		}
+	];*/
+	
 	
 
 	//set up ajax call
@@ -31,16 +32,19 @@
 		.fail(function(){
 			console.log('error');
 		});
-	//helper function
-	function intersection(singleBirdInfo){
-			var id = singleBirdInfo._id;
-			var setMatches =  _.intersection(singleBirdInfo.colors, selectedColors);
-			if(setMatches.length){
-				matchedSets.push(
-			 	[id, setMatches]
-			  );
-			}
-	}
+
+	//UNUSED helper function. I rewrote this in the data module. 
+	//Is it better to have in the module or out here?
+	// function intersection(singleBirdInfo){
+	// 	var id = singleBirdInfo._id;
+	// 	//check for common colors between the selected
+	// 	var commonColors =  _.intersection(singleBirdInfo.colors, selectedColors);
+	// 	if(commonColors.length){
+	// 		matchedSets.push(
+	// 	 	[id, setMatches]
+	// 	  );
+	// 	}
+	// }
 
 	//////// Cached data module //////////
 	//this function only fires after the ajax call has completed
@@ -66,21 +70,18 @@
 				}(),
 
 				findCommonColors = (function(){
-					var matchedSets = [];
+					var matchedForColor = [];
 
 					// for each of the items in birdData, call intersection
 					var get = function(gbd){
-						_.each(gbd.colors, function (singleBirdInfo){
-							var id = singleBirdInfo._id;
-							var setMatches =  _.intersection(singleBirdInfo.colors, selectedColors);
+						_.each(gbd, function (el, index){
+							var setMatches =  _.intersection(el.colors, selectedColors);
 								if(setMatches.length){
-									matchedSets.push(
-								 	[id, setMatches]
-								  );
+									matchedForColor.push(el);
 								}
 							}
 						);
-						return matchedSets;
+						return matchedForColor;
 					};
 					return {
 						get : get
@@ -95,8 +96,23 @@
 								matchedForSize.push(el);
 							}
 						});
-						console.log(matchedForSize);
 						return matchedForSize;
+					};
+					return {
+						get : get
+					};
+				}()),
+
+				filterByBehavior = (function(){
+					var matchedForBehavior = [];
+					var get = function(gbd){
+						_.each(gbd, function(el, index, list){
+							
+							if(el.behaviors == selectedBehavior){
+								matchedForBehavior.push(el);
+							}
+						});
+						return matchedForBehavior;
 					};
 					return {
 						get : get
@@ -106,32 +122,69 @@
 			return {
 				truncateSummary: truncateSummary,
 				findCommonColors: findCommonColors,
-				filterBySize: filterBySize
+				filterBySize: filterBySize,
+				filterByBehavior: filterByBehavior
 			};
 
 		}(); //end data - should there be an argument here?
 
-		//test output of modules
+///////////////// end data module ///////////////////////////////////////////////////
+
+	//output hardcoded selections to the screen for clarity		
+	arrayLogger(selectedSize, '#size-data-in');
+	arrayLogger(selectedColors, '#color-data-in');
+	arrayLogger(selectedBehavior, '#behavior-data-in');
+
+	//test output of module
+	//not sure why I need the param? Doesn't work without it
+
+	var sizeMatch = data.filterBySize.get(birdData);
+	var colorIntersection = data.findCommonColors.get(birdData);
+	var behaviorMatch = data.filterByBehavior.get(birdData);
+
+
+	//attach click handlers
+	$('#match-size').on("click", function(){
+		arrayLogger(sizeMatch, '#size-matches ul', 'commonName' );
+		$(this).detach();
+	});
+	$('#match-colors').on("click", function(){
+		arrayLogger(colorIntersection, '#color-matches ul', 'commonName');
+		$(this).detach();
+	});
+	$('#match-behavior').on("click", function(){
+		arrayLogger(behaviorMatch, '#behavior-matches ul', 'commonName');
+		$(this).detach();
+	});
 		
-		var allTiny = data.filterBySize.get(birdData);
-		arrayLogger(allTiny);
-		console.log(typeof allTiny);
-		console.log("get the excerpt: " + allTiny);
-			
-			// //test whether the caching works
-			// var excerpt2 = truncateSummary.getTruncate(globalBirdData);
-			// console.log("get the excerpt a second time: " + excerpt2);
+	/*test whether the caching works
+	var excerpt2 = truncateSummary.getTruncate(globalBirdData);
+	console.log("get the excerpt a second time: " + excerpt2);
 
-			// var matchedColors = findCommonColors.getMatchedSets(globalBirdData);
-			// console.log("matched sets: " + matchedColors);
+	var matchedColors = findCommonColors.getMatchedSets(globalBirdData);
+	console.log("matched sets: " + matchedColors);*/
 
-		}//end load program	
+	}
 
-	function arrayLogger(itemToLog){
-			_.each(itemToLog, function(el, index){
-				$('body').append('<div>' + el.commonName + '</div>');
+	//end load program	
+
+	function arrayLogger(itemToLog, whichDiv, prop ){
+		
+		if (typeof itemToLog == "object" || $.isArray(itemToLog)){
+			//for array or objects
+			_.each(itemToLog, function(el, index, list) {
+				if ( typeof el  == "object") {
+					$(whichDiv).append('<li>' + el[prop] + '</li>');
+				} else {
+					$(whichDiv).append('<li>' + el + '</li>');
+				}
 			});
+		}else{
+			//for string
+			$(whichDiv).append('<li>' + itemToLog + '</li>');
 		}
+	}
+
 })();
 	/*
 		
